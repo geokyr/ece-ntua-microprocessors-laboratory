cb89ba67b41a8576c904cc23734ac18d27b7508b
geokyr <imgeorgekyr@gmail.com>
2021-10-29 04:17:46 +0300

    added ex2

--- ex2/3.c
+++ ex2/3.c
@@ -1,44 +1,44 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-volatile unsigned char A, B, counter, flag = 0x01;
+volatile unsigned char A, B, counter, flag = 1;
 
 ISR(INT0_vect) {
-    A = PINA & 0x04;
-    B = PINB;
+    A = PINA & 0x04;                    // keep only PA2
+    B = PINB;                           // load input from B
 
-    if(A) {
-        counter = 0x00;
-        for (int i = 0; i < 8; i++) {
-            if(B & 0x01) {
-                counter++;
+    if(A) {                             // if PA2 is ON
+        counter = 0x00;                 // initialize counter
+        for (int i = 0; i < 8; i++) {   // for loop 8 times
+            if(B & 0x01) {              // if LSB is 1
+                counter++;              // increase counter
             }
-            B = B >> 1;
+            B = B >> 1;                 // rotate input right
         }
-        PORTC = counter;
+        PORTC = counter;                // output counter to C
     }
-    else {
-        counter = 0x00;
-        for (int i = 0; i < 8; i++) {
-            if(B & 0x01) {
-                counter = counter << 1;
-                counter++;
+    else {                              // if PA2 is OFF
+        counter = 0x00;                 // initialize counter
+        for (int i = 0; i < 8; i++) {   // for loop 8 times
+            if(B & 0x01) {              // if LSB is 1
+                counter = counter << 1; // shift counter left
+                counter++;              // increase counter
             }
-            B = B >> 1;
+            B = B >> 1;                 // rotate input right
         }
-        PORTC = counter;
+        PORTC = counter;                // output counter to C
     }
 }
 
 int main(void) {
-    MCUCR = 0x03;           // rising edge of INT0
-    GICR = 0x40;            // enable INT0
+    MCUCR = 0x03;                       // rising edge of INT0
+    GICR = 0x40;                        // enable INT0
 
-    DDRA = 0x00;            // input from A
-    DDRB = 0x00;            // input from B
-    DDRC = 0xFF;            // output at C
+    DDRA = 0x00;                        // input from A
+    DDRB = 0x00;                        // input from B
+    DDRC = 0xFF;                        // output at C
 
-    while(flag) {           // infinite loop
-        sei();              // enable interrupts
+    while(flag) {                       // infinite loop
+        sei();                          // enable interrupts
     }
 }
